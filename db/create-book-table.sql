e8a65c3f79f28a6da6955ffa76abf6219b42a67e
Dave Banerjee <justdave@Daves-MacBook-Air.local>
2019-05-05 21:16:35 -0500

    Get works Update in progress

--- db/create-book-table.sql
+++ db/create-book-table.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS books;
 
 -- Create book table
 CREATE TABLE IF NOT EXISTS books (
+	id int,
 	title varchar(30),
 	author varchar(30),
 	publisher varchar(30),
@@ -10,7 +11,7 @@ CREATE TABLE IF NOT EXISTS books (
 	checkedin boolean
 );
 
-INSERT INTO book.books VALUES ('Hyperion', 'Dan Simmons', 'Doubleday', '1989', '91', TRUE);
-INSERT INTO book.books VALUES ('The Fall of Hyperion', 'Dan Simmons', 'Doubleday', '1990', '92', FALSE);
-INSERT INTO book.books VALUES ('Endymion', 'Dan Simmons', 'Doubleday', '1996', '90', TRUE);
-INSERT INTO book.books VALUES ('The Rise of Endymion', 'Dan Simmons', 'Doubleday', '1997', '90', FALSE);
+INSERT INTO book.books VALUES ( 1, 'Hyperion', 'Dan Simmons', 'Doubleday', '1989', '91', TRUE);
+INSERT INTO book.books VALUES ( 2, 'The Fall of Hyperion', 'Dan Simmons', 'Doubleday', '1990', '92', FALSE);
+INSERT INTO book.books VALUES ( 3, 'Endymion', 'Dan Simmons', 'Doubleday', '1996', '90', TRUE);
+INSERT INTO book.books VALUES ( 4, 'The Rise of Endymion', 'Dan Simmons', 'Doubleday', '1997', '90', FALSE);
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -14,6 +16,7 @@ import (
 
 // Book is the struct containing the info
 type Book struct {
+	ID          int    `json:"id"`
 	Title       string `json:"title"`
 	Author      string `json:"author"`
 	Publisher   string `json:"publisher"`
@@ -30,7 +33,9 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", GetBooks)
-	// router.HandleFunc("/")
+	router.HandleFunc("/getbooks", GetBooks)
+	router.HandleFunc("/getbook/{id}", GetBook)
+	router.HandleFunc("/updatebook", UpdateBook)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
@@ -56,17 +61,20 @@ func GetBooks(w http.ResponseWriter, r *http.Request) {
 	results, err := db.Query("SELECT * FROM books")
 	if err != nil {
 		panic(err)
+		w.WriteHeader(404)
 	}
 
 	for results.Next() {
 		var title, author, publisher, publishdate string
-		var rating int
+		var id, rating int
 		var checkin bool
-		err = results.Scan(&title, &author, &publisher, &publishdate, &rating, &checkin)
+		err = results.Scan(&id, &title, &author, &publisher, &publishdate, &rating, &checkin)
 		if err != nil {
 			panic(err)
+			w.WriteHeader(404)
 		}
 
+		book.ID = id
 		book.Title = title
 		book.Author = author
 		book.Publisher = publisher
@@ -75,9 +83,79 @@ func GetBooks(w http.ResponseWriter, r *http.Request) {
 		book.Status = checkin
 		books = append(books, book)
 	}
-
-	// tmpl.ExecuteTemplate(w, "GetBooks", results)
 	json.NewEncoder(w).Encode(&books)
 	defer db.Close()
+	w.WriteHeader(200)
+}
+
+// GetBook grabs the book that
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+
+	id := mux.Vars(r)
+
+	results, err := db.Query("SELECT * FROM books WHERE id=?", id["id"])
+	if err != nil {
+		panic(err)
+		w.WriteHeader(404)
+	}
+
+	book := Book{}
+
+	for results.Next() {
+		var title, author, publisher, publishdate string
+		var id, rating int
+		var checkin bool
+		err := results.Scan(&id, &title, &author, &publisher, &publishdate, &rating, &checkin)
+		if err != nil {
+			panic(err)
+			w.WriteHeader(404)
+		}
+		book.ID = id
+		book.Title = title
+		book.Author = author
+		book.Publisher = publisher
+		book.PublishDate = publishdate
+		book.Rating = rating
+		book.Status = checkin
+	}
+	json.NewEncoder(w).Encode(&book)
+	defer db.Close()
+	w.WriteHeader(200)
+}
+
+// UpdateBook takes in a json payload and will overwrite any existing entry
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+
+	db := dbConn()
+
+	if r.Method == "POST" {
+
+		read, err := r.GetBody()
+
+		if err != nil {
+			panic(err)
+			w.WriteHeader(418)
+		}
+
+		body, err := ioutil.ReadAll(read)
+
+		book := Book{}
+
+		err = json.Unmarshal(body, &book)
+
+		query := fmt.Sprintf("INSERT INTO book.books VALUES (%d,%s,%s,%s,%s,%d,%t)", book.ID, book.Title, book.Author, book.Publisher, book.PublishDate, book.Rating, book.Status)
+
+		results, err := db.Prepare(query)
+		if err != nil {
+			panic(err)
+			w.WriteHeader(404)
+		}
+		results.Exec()
+		log.Printf("INSERT for %v Successful!", book)
+	}
+
+	defer db.Close()
+	w.WriteHeader(200)
 
 }
